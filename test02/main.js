3fb1dc56c863f1c458021896fd37d8fab0f417e3
brisk84 <brisk@inbox.ru>
2022-04-10 22:50:56 +0300

    first commit

---
+++ test02/main.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: "Socks",
+        altText: "A pair of socks",
+        image: "./vmSocks-blue-onWhite.jpg",
+        // image: "./vmSocks-green-onWhite.jpg",
+        socks_link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks"
+    }
+})
---
+++ test03/main.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: "Socks",
+        image: "./vmSocks-green-onWhite.jpg",
+        inStock: true,
+        inventory: 10,
+        onSale: true
+    }
+})
