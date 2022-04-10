03e2fc9def6905beb1131673ea7dee34bb6d2c22
brisk84 <brisk@inbox.ru>
2022-04-10 23:45:55 +0300

    feat: test06

---
+++ test06/main.js
@@ -0,0 +1,30 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+      product: "Socks",
+      image: "./vmSocks-green-onWhite.jpg",
+      inStock: false,
+      details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+      variants: [
+        {
+          variantId: 2234,
+          variantColor: "green",
+          variantImage: "./vmSocks-green-onWhite.jpg"    
+        },
+        {
+          variantId: 2235,
+          variantColor: "blue",
+          variantImage: "./vmSocks-blue-onWhite.jpg" 
+        }
+      ],
+      cart: 0
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1
+    },
+    updateProduct(variantImage) {
+      this.image = variantImage
+    }
+  }
+})
